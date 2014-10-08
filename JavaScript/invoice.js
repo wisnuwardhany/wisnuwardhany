001e668a4ebef1d6ec1a506a6aa6cbe064813d02
wisnuwardhany@gmail.com <wisnuwardhany@gmail.com@fbbd2181-66e3-e6d1-39d6-11bebabf0ac3>
2014-10-08 08:36:41 +0000

    Email Invoice

--- JavaScript/invoice.js
+++ JavaScript/invoice.js
@@ -12,7 +12,7 @@ document.write('</div>');
 if(t == 1)
 	document.write('<a href="#kontactr-form" rel="facebox">'+ctxt+'</a>');
 else if(t == 2)
-	document.write('<a href="#kontactr-form" rel="facebox"><img src="http://3.bp.blogspot.com/-j8vyDh441i8/VCj1_j1UanI/AAAAAAAABE4/HCr-As39OZs/s1600/klik-disini11.gif height="80" width="150" alt="Order Now" /></a>');
+	document.write('<a href="#kontactr-form" rel="facebox"><img src="http://3.bp.blogspot.com/-j8vyDh441i8/VCj1_j1UanI/AAAAAAAABE4/HCr-As39OZs/s1600/klik-disini11.gif height="70" width="120" alt="Order Now" /></a>');
 else if(t == 3)
 	document.write('<a href="#kontactr-form" rel="facebox"><img src="http://kontactr.com/pics/big.gif" alt="Contact me" /></a>');
 else
